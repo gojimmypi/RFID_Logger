8179218a8f5ebc733169d25f4d8cc8b03cf36215
gojimmypi <gojimmypi@gmail.com>
2020-06-10 14:24:12 -0700

    remove unused default template items

--- Web/RFID_aspx_Webform_App/Global.asax.cs
+++ Web/RFID_aspx_Webform_App/Global.asax.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace RFID
 {
@@ -14,8 +8,6 @@ namespace RFID
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
